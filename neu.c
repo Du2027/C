77df8ec92833572a334d1cec66e43c45cbeaea65
P.-L <104651353+Du2072@users.noreply.github.com>
2023-09-16 17:35:29 +0200

    added a few thngs

--- neu.c
+++ neu.c
@@ -1,5 +1,33 @@
 #include<stdio.h>
 int main(){
-    printf("Hello, Wod");
+    int age = 20;
+    printf("Age is %d", age);
+    age = 21;
+    printf("\nNew: Age %d", age);
+
+    /*
+    int     4bytes  %d
+    double  8bytes  %lf
+    float   4bytes  %f
+    char    1byte   %c
+    */
+
+    double Number_double = 12.35;
+    printf("\n%.2lf",Number_double); //.2lf for 2 decimal points. standart = 6 decimals
+    float Number_Float = 22.1f;
+    printf("\n%.1f",Number_Float);   //same for float
+
+    printf("\nSize of a float: %d", sizeof(float)); //gives the datatype size in bytes
+
+    int num1 = 11;
+    int num2 = 22;
+    printf("\n%d\n", num1+num2);
+
+    int your_Age;
+    scanf("%d", &your_Age);
+    printf("your Age: = %d",your_Age);
+    scanf("%d");
+    //scanf("%d %c",var1,var2) also possible
+
     return 0;
 }
