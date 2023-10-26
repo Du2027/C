0579224bb0248e80079d0f07c43b639bbd490881
Rasbn <player1.normal@gmail.com>
2023-10-26 18:13:25 +0200

    lol

--- Rasbi/simple_Button.c
+++ Rasbi/simple_Button.c
@@ -1,39 +1,51 @@
 #include<wiringPi.h>
 #include<stdio.h>
-#define Gpio17 0
-#define Gpio18 1
-#define Gpio23 4
-#define Gpio24 5
-#define Gpio27 2
-#define Gpio22 3
+
+#define Gpio1_IN 0
+#define Gpio2_IN 1
+#define Gpio3_IN 2
+
+#define Gpio1_OUT 3
+#define Gpio2_OUT 4
+#define Gpio3_OUT 5
 
 int main(){
-    if(wiringPiSetup()==-1){
-        return -1;
+    if(wiringPiSetup()== -1){
+        return 1;
     }
-    pinMode(Gpio17,OUTPUT);
-    pinMode(Gpio27,OUTPUT);
-    pinMode(Gpio22,OUTPUT);
-    pinMode(Gpio18,INPUT);
-    pinMode(Gpio23,INPUT);
-    pinMode(Gpio24,INPUT);
 
-    digitalWrite(Gpio17,LOW);
+    int i;
+    for(i=0;i<6;i++){
+        if(i<3){
+            pinMode(i,INPUT);
+            printf("IN : %d\n",i);
+        }
+        else{
+            pinMode(i,OUTPUT);
+            printf("OUT : %d\n",i);
+        }
+    }
 
+    int Color_array[]={LOW,LOW,LOW};
     while(1){
-    if(digitalRead(Gpio18)==0 || digitalRead(Gpio23)==0 || digitalRead(Gpio24)==0){
-        digitalWrite(Gpio17,HIGH);
-    }
-    if(digitalRead(Gpio23)==0){
-        digitalWrite(Gpio27,HIGH);
-    }
-    if(digitalRead(Gpio24)==0){
-        digitalWrite(Gpio22,HIGH);
-    }
-    else{
-        digitalWrite(Gpio17,LOW);
-        digitalWrite(Gpio27,LOW);
-        digitalWrite(Gpio22,LOW);
-    }}
-    return 0;
+        for(i=0;i<3;i++){
+            Color_array[i]=LOW;
+        }
+        if(digitalRead(Gpio1_IN)==0){
+            Color_array[0] = HIGH;
+        }
+        if(digitalRead(Gpio2_IN)==0){
+            Color_array[1] = HIGH;
+        }
+        if(digitalRead(Gpio3_IN)==0){
+            Color_array[2] = HIGH;
+        }
+
+        digitalWrite(Gpio1_OUT,Color_array[0]);
+        digitalWrite(Gpio2_OUT,Color_array[1]);
+        digitalWrite(Gpio3_OUT,Color_array[2]);
+
+        printf("COLOR : %d%d%d\n", Color_array[0], Color_array[1], Color_array[2]);
+        }
+  return 0;
 }
