8c6732acd4a1f56c66290c35b38ab20041120e48
Rasbn <player1.normal@gmail.com>
2023-10-27 18:39:06 +0200

    working colorlow

--- Rasbi/Colorflow.c
+++ Rasbi/Colorflow.c
@@ -1,44 +1,82 @@
+#include<stdio.h>
+#include<wiringPi.h>
+#include<softPwm.h>
+
 #define GPIO1 0;
 #define GPIO2 1;
 #define GPIO3 2;
 
 
 int colorset(int r,int g,int b){
-  digitalWrite(0, r);
-  digitalWrite(1, g);
-  digitalWrite(2, b);
+  softPwmWrite(0, r);
+  softPwmWrite(1, g);
+  softPwmWrite(2, b);
 }
-int main(){
-  if(wiringPiSetup()==-1){
-    printf("Wiring Pi Setup failed\n");
+int higher(){
+    int i;
+    for(i=0;i<256;i++){
+        colorset(i,0,0);
+        delay(10);
+    }
+    for(i=256;i>0;i--){
+        colorset(i,0,0);
+        delay(10);
+    }
+    for(i=0;i<256;i++){
+        colorset(0,i,0);
+        delay(10);
+    }
+    for(i=256;i>0;i--){
+        colorset(0,i,0);
+        delay(10);
+    }
+    for(i=0;i<256;i++){
+        colorset(0,0,i);
+        delay(10);
+    }
+    for(i=256;i>0;i--){
+        colorset(0,0,i);
+        delay(10);
+    }
+    for(i=0;i<256;i++){
+        colorset(0,i,i);
+        delay(10);
+    }
+    for(i=256;i>0;i--){
+        colorset(0,i,i);
+        delay(10);
+    }
+    for(i=0;i<256;i++){
+        colorset(i,i,0);
+        delay(10);
+    }
+    for(i=256;i>0;i--){
+        colorset(i,i,0);
+        delay(10);
+    }
+    for(i=0;i<256;i++){
+        colorset(i,i,i);
+        delay(10);
+    }
+    for(i=256;i>0;i--){
+        colorset(i,i,i);
+        delay(10);
     }
 
-  int i;
-  for(i=0;i<4;i++){
-    pinMode(i, OUTPUT);
-    printf("OUTPUT : %d\n",i);
-  }
-  
-  while(1){
-    for(i=0;i<256;i++){
-      colorset(i,0,0);
-    }delay(1000);
-    for(i=0;i<256;i++){
-      colorset(0,i,0);
-    }delay(1000);
-    for(i=0;i<256;i++){
-      colorset(0,0,i);
-    }delay(1000);
-    for(i=0;i<256;i++){
-      colorset(i,i,0);
-    }delay(1000);
-    for(i=0;i<256;i++){
-      colorset(0,i,i);
-    }delay(1000);
-    for(i=0;i<256;i++){
-      colorset(i,i,i);
-    }delay(1000);
-  }
-    return 1;
-  }
 }
+
+int main(){
+    if(wiringPiSetup()==-1){
+        printf("Wiring Pi Setup failed\n");
+    }
+
+    int i;
+    for(i=0;i<4;i++){
+        softPwmCreate(i,0,100);
+        printf("OUTPUT : %d\n",i);
+    }
+  
+    higher();
+    return 1;
+}
+
