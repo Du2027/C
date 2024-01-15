b5951bef680cf70a077004ac581c021da10e5225
Rasbi <player1.buissnes@gmail.com>
2024-01-15 14:22:17 +0100

    reorganized directories

---
+++ CLI-Todo/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+    printf("\033[32mLOLOLOLOLOO     \033[0m");
+    printf("\u2612\n");
+    printf("TEXTETXTDTTD\n");
+}
