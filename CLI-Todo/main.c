7a64896ad3c4846ff5f55a9553a1399cbe7e3c54
Rasbi <player1.buissnes@gmail.com>
2024-02-19 17:57:42 +0100

    Added rename function
    added rename function and moved the Entrys file to /home/p/.CLI_TODO instead of the current dir

--- CLI-Todo/main.c
+++ CLI-Todo/main.c
@@ -3,9 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void list_Todos(FILE *entrys){
-    printf("Todo's --------------------------\n");   
-
+void list(FILE *entrys){
     char cont[100];
     int line_num = 1;
     while(fgets(cont, 100, entrys)){
@@ -26,23 +24,23 @@ int main(int argc, char *argv[]){
 
     // Crash if no arguments where given
     if(argc == 1){
-        printf("add -help for help \n");
+        printf("Incorrect argument count, add -help for help \n");
         return -1;
     }
 
     // Crash if too many arguments where given
-    if(argc > 3){
-        printf("too many arguments given\n");
+    if(argc > 3 && strcmp(argv[1], "-attach_category") == 1 && strcmp(argv[1], "-rename") == 1){
+        printf("Incorrect argument count\n");
         return -1;
     }
 
     // Open Entrys.txt, if it doesnt exsist, creating
-    FILE *entrys = fopen("Entrys.txt", "r");
+    FILE *entrys = fopen("/home/p/.CLI_TODO/Entrys.txt", "r");
     if(entrys == NULL){
-        entrys = fopen("Entrys.txt", "w");
+        entrys = fopen("/home/p/.CLI_TODO/Entrys.txt", "w");
     }
     fclose(entrys);
-    entrys = fopen("Entrys.txt", "a+");
+    entrys = fopen("/home/p/.CLI_TODO/Entrys.txt", "a+");
     
     // Help command
     if(strcmp(argv[1], "-help") == 0){
@@ -52,12 +50,17 @@ int main(int argc, char *argv[]){
 
     // List command
     if(strcmp(argv[1], "-list") == 0 || strcmp(argv[1], "-l") == 0){
-        list_Todos(entrys);
+        printf("Todo's --------------------------\n"); 
+        list(entrys);
         command = 0;
     }
     
     // Add command
     if(strcmp(argv[1], "-add" ) == 0){
+        if(argc == 2){
+            printf("Incorrect argument count\n");
+            return -1;
+        }
         final_str = malloc(strlen(argv[2])+ sizeof(char)*2);
         final_str = argv[2];
         strcat(final_str, "\n");
@@ -68,7 +71,7 @@ int main(int argc, char *argv[]){
     // Remove command
     if(strcmp(argv[1], "-rm") == 0){
         char buffer[1000];
-        FILE *tmpf = fopen("tmp__Entrys.txt", "w"); // creating temp file
+        FILE *tmpf = fopen("/home/p/.CLI_TODO/tmp__Entrys.txt", "w"); // creating temp file
         char *line = argv[2];
         int line_to_rm = *line - '0'; // storing witch line to remove
         bool end_of_file = false;
@@ -77,9 +80,14 @@ int main(int argc, char *argv[]){
         // Cloning Entrys.txt to tmp__Entrys.txt without removed line
         do{
             fgets(buffer, 1000, entrys);
+            if(current_line == line_to_rm){
+                printf("Excludet line : %d : %s",line_to_rm, buffer);
+            }
+
             if(feof(entrys)){
                 end_of_file = true;
             }
+
             else if(current_line != line_to_rm){
                 fputs(buffer, tmpf);
             }
@@ -92,10 +100,65 @@ int main(int argc, char *argv[]){
         fclose(tmpf);
 
         // Removing Entrys.txt and renaming tmp file to Entrys
-        remove("Entrys.txt");
-        rename("tmp__Entrys.txt", "Entrys.txt");
+        remove("/home/p/.CLI_TODO/Entrys.txt");
+        rename("/home/p/.CLI_TODO/tmp__Entrys.txt", "/home/p/.CLI_TODO/Entrys.txt");
         command = 0;
         
+    }
+
+    if(strcmp(argv[1], "-attach_category") == 0){
+        command = 0;
+        if(argc != 4){
+            printf("Incorrect argument count");
+            return -1;
+        }
+    }
+
+    if(strcmp(argv[1], "-detach_category") == 0){
+        command = 0;
+    }
+
+    // Rename Command
+    if(strcmp(argv[1], "-rename") == 0){
+        command = 0;
+        if(argc != 4){
+            printf("Incorrect argument count\n");
+            return -1;
+        }
+
+        FILE *TMP = fopen("/home/p/.CLI_TODO/tmp__Entrys.txt", "w");
+        char buffer[1000];
+        int line = 1;
+        char *Line = argv[2];
+        int line_to_rn = *Line - '0';
+        bool END = false;
+        char Stinggggg[sizeof(argv[3])+sizeof(char)*2];
+        strcat(Stinggggg, argv[3]);
+        strcat(Stinggggg, "\n");
+
+        do{
+            fgets(buffer, 1000, entrys);
+
+            if(feof(entrys)){
+                END = true;
+            }
+            
+            else if(line != line_to_rn){
+                fputs(buffer, TMP);
+            }
+
+            else if(line == line_to_rn){
+                fputs(Stinggggg, TMP);
+            }
+            line++;
+        }
+        while(END != true);
+
+        fclose(TMP);
+        fclose(entrys);
+
+        remove("/home/p/.CLI_TODO/Entrys.txt");
+        rename("/home/p/.CLI_TODO/tmp__Entrys.txt", "/home/p/.CLI_TODO/Entrys.txt");
     }
 
     // If command was unknown crash
